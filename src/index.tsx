d3f80d78b8c0a79475bc6f06b600bb8b482c87be
AdrianDanlos <danlosadrian@gmail.com>
2024-04-01 03:32:03 +0800

    rutas actualizadas

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.scss";
 import PageContainer from "./components/PageContainer/pageContainer";
 import { Navbar } from "./components/PageContainer/Navbar/navbar";
 import { HeroSection } from "./components/PageContainer/HeroSection/heroSection";
-import { Companies } from "./components/Companies/companies";
+import { Companies } from "./components/PageContainer/Companies/companies";
 import { Contact } from "./components/PageContainer/Contact/contact";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
