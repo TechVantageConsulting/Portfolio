4f4245d55dec689b6a82935b87e7fedcd5b77730
massije <jedc.marti@gmail.com>
2024-03-31 14:09:01 +0200

    follow-footer

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import PageContainer from "./components/PageContainer/PageContainer";
+import { Footer } from "./components/PageContainer/Footer/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PageContainer>
       <div>Components</div>
+      <Footer/>
     </PageContainer>
   </React.StrictMode>
 );
