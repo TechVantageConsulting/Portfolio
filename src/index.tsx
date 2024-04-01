1f95c9a2bd724f0e226bb25baf91f93ca287005a
massije <jedc.marti@gmail.com>
2024-04-01 23:02:45 +0200

    Follow-Services-Section

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,20 @@ import { HeroSection } from "./components/PageContainer/HeroSection/heroSection"
 import { Companies } from "./components/PageContainer/Companies/companies";
 import { Contact } from "./components/PageContainer/Contact/contact";
 import { Footer } from "./components/PageContainer/Footer/footer";
+import { Services } from "./components/PageContainer/Services/services";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // TODO: REMOVE
   <React.StrictMode>
     <PageContainer>
       <Navbar />
-      <HeroSection />
+      {/* <HeroSection /> */}
     </PageContainer>
-    <Companies />
-    <PageContainer>
-      <Contact />
-      <Footer />
-    </PageContainer>
+    {/* <Companies /> */}
+    {/* <PageContainer> */}
+    {/* <Services /> */}
+    {/* <Contact /> */}
+    {/* <Footer /> */}
+    {/* </PageContainer> */}
   </React.StrictMode>
 );
